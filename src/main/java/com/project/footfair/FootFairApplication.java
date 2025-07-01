276b1ed7f97de4b07605fc529958df7673847788
Daniel <danielsouzanovo@gmail.com>
2025-07-01 01:17:07 -0300

    chore: setup project with initial structure and dev environment

---
+++ src/main/java/com/project/footfair/FootFairApplication.java
@@ -0,0 +1,14 @@
+package com.project.footfair;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FootFairApplication {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        SpringApplication.run(FootFairApplication.class, args);
+    }
+
+}
