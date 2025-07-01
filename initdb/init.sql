b8bc4cb0b5cf0a11eaec74ed9175b2286604779f
Daniel <danielsouzanovo@gmail.com>
2025-07-01 01:18:31 -0300

    chore: define initial database schema

---
+++ initdb/init.sql
@@ -0,0 +1,55 @@
+CREATE DATABASE IF NOT EXISTS footflair;
+
+USE footflair;
+
+CREATE TABLE IF NOT EXISTS players (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    phone VARCHAR(20) NOT NULL,
+    role TINYINT NOT NULL DEFAULT 1,
+    squad_id INT,
+    speed TINYINT CHECK (speed BETWEEN 0 AND 100),
+    passing TINYINT CHECK (passing BETWEEN 0 AND 100),
+    shooting TINYINT CHECK (shooting BETWEEN 0 AND 100),
+    defense TINYINT CHECK (defense BETWEEN 0 AND 100),
+    stamina TINYINT CHECK (stamina BETWEEN 0 AND 100)
+
+    FOREIGN KEY (squad_id) REFERENCES squads(id)
+);
+
+CREATE TABLE IF NOT EXISTS positions (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(50) NOT NULL UNIQUE
+);
+
+CREATE TABLE IF NOT EXISTS positions_player (
+    player_id INT NOT NULL,
+    position_id INT NOT NULL,
+    PRIMARY KEY (player_id, position_id),
+
+    FOREIGN KEY (player_id) REFERENCES players(id),
+    FOREIGN KEY (position_id) REFERENCES positions(id)
+);
+
+CREATE TABLE IF NOT EXISTS squads (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    code VARCHAR(20) NOT NULL UNIQUE,
+    name VARCHAR(100) NOT NULL,
+    create_player_id INT NOT NULL,
+    criado_em TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+
+    FOREIGN KEY (create_player_id) REFERENCES player(id)
+    );
+
+CREATE TABLE IF NOT EXISTS games (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    date DATETIME NOT NULL,
+    location VARCHAR(100) NOT NULL,
+    squad_id INT NOT NULL UNIQUE
+
+    FOREIGN KEY (squad_id) REFERENCES squads(id)
+)
+
+
+
+
