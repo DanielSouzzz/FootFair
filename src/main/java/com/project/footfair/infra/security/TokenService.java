036a4b1edef2df05a0d5e0a1c38769974c4b7e2f
Daniel <danielsouzanovo@gmail.com>
2025-07-06 16:03:22 -0300

    feat: implementar validacao de token

--- src/main/java/com/project/footfair/infra/security/TokenService.java
+++ src/main/java/com/project/footfair/infra/security/TokenService.java
@@ -3,6 +3,7 @@ package com.project.footfair.infra.security;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
 import com.project.footfair.entity.Player;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -30,6 +31,20 @@ public class TokenService {
             throw new RuntimeException("Error while authenticating!");
         }
     }
+
+    public String validateToken(String token){
+        try{
+            Algorithm algorithm = Algorithm.HMAC256(secretKey);
+            return JWT.require(algorithm)
+                    .withIssuer("footFair")
+                    .build()
+                    .verify(token)
+                    .getSubject();
+        } catch (JWTVerificationException exception) {
+            return null;
+        }
+    }
+
     private Instant generateExpirationDate() {
         return LocalDateTime.now()
                 .plusHours(2)
