0b705470bced5d3b0bc8faeef5748f96a5325116
Daniel <danielsouzanovo@gmail.com>
2025-07-19 19:53:13 -0300

    refactor: Adicionar classe entidade squad

--- src/main/java/com/project/footfair/entity/Squad.java
+++ src/main/java/com/project/footfair/entity/Squad.java
@@ -1,4 +1,27 @@
 package com.project.footfair.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.*;
+
+@Entity
+@Table(name = "squad")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(exclude = "squads")
 public class Squad {
+
+    @Id
+    private Long id;
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
 }
