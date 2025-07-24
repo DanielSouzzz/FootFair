a730a5d6b29ac921ca014a2a2f473278bdfa3312
Daniel <danielsouzanovo@gmail.com>
2025-07-23 23:49:00 -0300

    feat: adicionar logica de bloqueio ip.

--- src/main/java/com/project/footfair/service/LoginAttemptService.java
+++ src/main/java/com/project/footfair/service/LoginAttemptService.java
@@ -2,9 +2,55 @@ package com.project.footfair.service;
 
 import org.springframework.stereotype.Component;
 
+import java.time.LocalDateTime;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
 @Component
 public class LoginAttemptService {
+    private static final int MAX_ATTEMPTS = 5;
+    private static final long BLOCK_DURATION_MINUTES = 10;
+
+    private final Map<String, AttemptInfo> attempts = new ConcurrentHashMap<>();
+
+    public void loginFailed(String ip){
+        AttemptInfo info = attempts.getOrDefault(ip, new AttemptInfo());
+    }
+
+    public void loginSucceeded(String ip){
+        attempts.remove(ip);
+    }
+
     public boolean isBlocked(String ip) {
+        AttemptInfo info = attempts.get(ip);
+        if (info == null){
+            return false;
+        }
+
+        if(info.getAttempts() >= MAX_ATTEMPTS){
+            if (info.getLastAttempt().plusMinutes(BLOCK_DURATION_MINUTES).isAfter(LocalDateTime.now())){
+                return true;
+            }else {
+                attempts.remove(ip);
+            }
+        }
         return false;
+    }
+
+    private static class AttemptInfo{
+        private int attempts = 0;
+        private LocalDateTime lastAttempt = LocalDateTime.now();
+
+        void increment(){
+            attempts++;
+            lastAttempt = LocalDateTime.now();
+        }
+        int getAttempts() {
+            return attempts;
+        }
+
+        LocalDateTime getLastAttempt() {
+            return lastAttempt;
+        }
     }
 }
