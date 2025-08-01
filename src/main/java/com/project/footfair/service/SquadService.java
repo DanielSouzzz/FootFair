269f70ff41f6a26346beb670c3f445dbd77d1639
Daniel <danielsouzanovo@gmail.com>
2025-08-01 20:25:44 -0300

    feat(service): adicionar verificacao de usuario

--- src/main/java/com/project/footfair/service/SquadService.java
+++ src/main/java/com/project/footfair/service/SquadService.java
@@ -1,18 +1,26 @@
 package com.project.footfair.service;
 
+import com.project.footfair.entity.Player;
 import com.project.footfair.entity.Squad;
+import com.project.footfair.repository.PlayerRepository;
 import com.project.footfair.repository.SquadRepository;
+import jakarta.validation.ValidationException;
 import org.springframework.stereotype.Service;
 
 @Service
 public class SquadService {
     private final SquadRepository squadRepository;
+    private final PlayerRepository playerRepository;
 
-    public SquadService(SquadRepository squadRepository) {
+    public SquadService(SquadRepository squadRepository, PlayerRepository playerRepository) {
         this.squadRepository = squadRepository;
+        this.playerRepository = playerRepository;
     }
 
     public Squad createSquad(Squad squad){
+        playerRepository.findById(squad.getCreate_player_id())
+                .orElseThrow(()-> new ValidationException("User not found!"));
+
         Squad newSquad = squadRepository.save(squad);
         return newSquad;
     }
