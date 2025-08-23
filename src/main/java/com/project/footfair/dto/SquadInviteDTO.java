ad560d201aebb9cf8f96a07763e02524d2d4178f
Daniel <danielsouzanovo@gmail.com>
2025-08-23 15:33:08 -0300

    feat(dto): adicionar novo dto para retornar link e code do squad

---
+++ src/main/java/com/project/footfair/dto/SquadInviteDTO.java
@@ -0,0 +1,12 @@
+package com.project.footfair.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class SquadInviteDTO {
+    private String code;
+    private String inviteLink;
+}
