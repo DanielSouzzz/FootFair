15987ec585b7071bd1d74bd8be4facf253a37eab
Daniel <danielsouzanovo@gmail.com>
2025-08-25 23:06:15 -0300

    feat(dto): adicionar dto de response para se juntar ao squad.

--- src/main/java/com/project/footfair/dto/JoinSquadResponseDTO.java
+++ src/main/java/com/project/footfair/dto/JoinSquadResponseDTO.java
@@ -1,4 +1,16 @@
 package com.project.footfair.dto;
 
+import com.project.footfair.entity.Player;
+import com.project.footfair.entity.Squad;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class JoinSquadResponseDTO {
+    private Player dataPlayer;
+    private Squad dataSquad;
+    private String message;
 }
