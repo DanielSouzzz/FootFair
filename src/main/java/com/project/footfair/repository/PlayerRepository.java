19b535485a25ed411b2ab01c95657ab2aa922317
Daniel <danielsouzanovo@gmail.com>
2025-08-25 23:50:26 -0300

    feat(repository): adicionar metodo para adicionar player ao squad.

--- src/main/java/com/project/footfair/repository/PlayerRepository.java
+++ src/main/java/com/project/footfair/repository/PlayerRepository.java
@@ -2,6 +2,7 @@ package com.project.footfair.repository;
 
 import com.project.footfair.entity.Player;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
@@ -14,4 +15,10 @@ public interface PlayerRepository extends JpaRepository<Player, Long> {
 
     @Query(value = "select exists(select 1 from player_squad ps where ps.player_id = :player_id)", nativeQuery = true)
     Boolean findPlayerInSquadById(@Param("player_id") Long player_id);
+
+    @Modifying
+    @Query(value = "insert into player_squad (player_id, squad_id) values (:player_id, :squad_id)", nativeQuery = true)
+    void joinPlayerInSquad(@Param("playerId") Long playerId, @Param("squadId") Long squadId);
 }
+
+
