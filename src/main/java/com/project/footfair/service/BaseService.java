c4a511d345cc7e53623c14c21ea372190f60aebc
Daniel <danielsouzanovo@gmail.com>
2025-08-25 23:03:14 -0300

    fix(service): adicionar anotacao na classe service para ser carregada pelo spring

--- src/main/java/com/project/footfair/service/BaseService.java
+++ src/main/java/com/project/footfair/service/BaseService.java
@@ -2,7 +2,9 @@ package com.project.footfair.service;
 
 import jakarta.validation.ValidationException;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Component;
 
+@Component
 public abstract class BaseService {
     protected <T, ID> T findEntityById(CrudRepository<T, ID> repository, ID id, String entityName){
         return repository.findById(id)
