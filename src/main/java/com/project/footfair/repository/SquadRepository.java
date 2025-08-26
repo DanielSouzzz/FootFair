eb064d8ab7e6dab51483afafbf792ba2c9c6cd11
Daniel <danielsouzanovo@gmail.com>
2025-08-25 23:51:56 -0300

    feat(repository): adicionar query para verificar se o convite e valido.

--- src/main/java/com/project/footfair/repository/SquadRepository.java
+++ src/main/java/com/project/footfair/repository/SquadRepository.java
@@ -2,8 +2,15 @@ package com.project.footfair.repository;
 
 import com.project.footfair.entity.Squad;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.Optional;
 
 @Repository
 public interface SquadRepository extends JpaRepository <Squad, Long>{
+
+    @Query(value = "select * from player p where p.invite_code=:code limit 1", nativeQuery = true)
+    Optional<Squad> findInviteCode(PathVariable code);
 }
