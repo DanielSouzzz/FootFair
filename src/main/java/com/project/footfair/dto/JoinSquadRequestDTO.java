8a08b9c23b12d51bcec7b7519dbbdf51b772d5e9
Daniel <danielsouzanovo@gmail.com>
2025-08-25 23:06:05 -0300

    feat(dto): adicionar dto de request para se juntar ao squad.

--- src/main/java/com/project/footfair/dto/JoinSquadRequestDTO.java
+++ src/main/java/com/project/footfair/dto/JoinSquadRequestDTO.java
@@ -1,4 +1,18 @@
 package com.project.footfair.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class JoinSquadRequestDTO {
+    @NotBlank
+    private String name;
+    @NotBlank
+    @Email
+    private String email;
 }
