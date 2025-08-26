3c6e79356778b2b29950d1997f12fd523cfc410a
Daniel <danielsouzanovo@gmail.com>
2025-08-25 23:11:34 -0300

    feat(repository): adicionar repository do player.

--- src/main/java/com/project/footfair/repository/PlayerRepository.java
+++ src/main/java/com/project/footfair/repository/PlayerRepository.java
@@ -2,6 +2,8 @@ package com.project.footfair.repository;
 
 import com.project.footfair.entity.Player;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -9,4 +11,7 @@ import java.util.Optional;
 @Repository
 public interface PlayerRepository extends JpaRepository<Player, Long> {
     Optional<Player> findByEmail(String email);
+
+    @Query(value = "select exists(select 1 from player_squad ps where ps.player_id = :player_id)", nativeQuery = true)
+    Boolean findPlayerInSquadById(@Param("player_id") Long player_id);
 }
