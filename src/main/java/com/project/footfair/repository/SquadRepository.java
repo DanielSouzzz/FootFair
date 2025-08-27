262a624010d3974f0abf2381dfb527de40f62c1f
Daniel <danielsouzanovo@gmail.com>
2025-08-26 23:56:13 -0300

    fix(reporitory): correcoes na query e tipagem.

--- src/main/java/com/project/footfair/repository/SquadRepository.java
+++ src/main/java/com/project/footfair/repository/SquadRepository.java
@@ -3,6 +3,7 @@ package com.project.footfair.repository;
 import com.project.footfair.entity.Squad;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.PathVariable;
 
@@ -11,6 +12,6 @@ import java.util.Optional;
 @Repository
 public interface SquadRepository extends JpaRepository <Squad, Long>{
 
-    @Query(value = "select * from player p where p.invite_code=:code limit 1", nativeQuery = true)
-    Optional<Squad> findInviteCode(PathVariable code);
+    @Query(value = "select * from squads p where p.invite_code=:code limit 1", nativeQuery = true)
+    Optional<Squad> findInviteCode(@Param("code") String code);
 }
